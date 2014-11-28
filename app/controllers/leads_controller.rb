e0b348e9863c5e476b7346481a8ff751ffc6d894
Christian Hein <chrishein@gmail.com>
2014-11-28 15:11:59 -0300

    Create Leads

---
+++ app/controllers/leads_controller.rb
@@ -0,0 +1,34 @@
+class LeadsController < ApplicationController
+  respond_to :json
+
+  def index
+    respond_with Lead.all
+  end
+
+  def show
+    respond_with lead
+  end
+
+  def create
+    respond_with Lead.create(lead_params) # :api, :v1,
+  end
+
+  def update
+    respond_with lead.update(lead_params)
+  end
+
+  def destroy
+    respond_with lead.destroy
+  end
+
+  private
+
+  def lead
+    Lead.find(params[:id])
+  end
+
+  def lead_params
+    params.require(:lead).permit(:first_name, :last_name, :email, :phone, :status, :notes)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :leads
+
   root to: 'home#index'
 
   get '*path', to: 'home#index'
