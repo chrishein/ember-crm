6f6585e9c0e0b9a42368b69397d5705f4d70f715
Christian Hein <chrishein@gmail.com>
2014-11-28 16:15:37 -0300

    Show selected Lead on the right

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,7 +1,9 @@
 // For more information see: http://emberjs.com/guides/routing/
 
 App.Router.map(function() {
-  this.resource('leads', { path: '/' })
+  this.resource('leads', { path: '/' }, function() {
+    this.resource('lead', { path: '/leads/:id' });
+  });
 });
 
 App.Router.reopen({
