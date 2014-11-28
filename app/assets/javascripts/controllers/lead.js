9e80e9d5cd4260a0d3ea737e3a6ee2ed03ddc771
Christian Hein <chrishein@gmail.com>
2014-11-28 17:42:05 -0300

    Edit all fields

--- app/assets/javascripts/controllers/lead.js
+++ app/assets/javascripts/controllers/lead.js
@@ -1,9 +1,10 @@
 App.LeadController = Ember.ObjectController.extend({
+  isEditing: false,
 
   showUnsavedMessage: function() {
     return this.get('isDirty') && !this.get('isSaving')
   }.property('isDirty', 'isSaving'),
-  
+
   actions: {
     saveChanges: function() {
       this.get('model').save();
